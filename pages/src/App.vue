b3dd42d8ffe46b7bb74749a4a75b98e828b14c75
a114383 <A114383@progressive.com>
2021-04-21 09:29:22 -0400

    adding vue need to cleaenup later

---
+++ pages/src/App.vue
@@ -0,0 +1,33 @@
+<!-- App.vue -->
+
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-app-bar color="red" app>
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+        <!-- each row contains 12 columns -->
+        <v-row>
+          <v-col cols="2"> </v-col>
+          <v-col cols="8">
+            <div id="content"><p>Place Notes Here</p></div>
+          </v-col>
+          <v-col cools="2"> </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer color="red" app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
